288a18474f8d31d647bf21861335f400ee9338e3
ethanwangsihan <ethan.wang.sihan@hotmail.com>
2020-12-08 22:16:17 +0800

    update

--- Christmas_machine/music_light_show/music_light_show.ino
+++ Christmas_machine/music_light_show/music_light_show.ino
@@ -36,6 +36,12 @@ int melody[] = {
   RE_H, MI_H, RE_H, DO_H, SOL_H,  SOL_M,  SOL_M,  SOL_M, LA_M, RE_H, SI_M, DO_H
 
 };
+int melody1[] = {
+  MI_M,MI_M,MI_M,MI_M,MI_M,MI_M,MI_M,SOL_M,DO_M,RE_M,MI_M,
+  FA_M,FA_M,FA_M,FA_M,FA_M,MI_M,MI_M,MI_M,MI_M,MI_M,RE_M,RE_M,DO_M,RE_M,SOL_M,
+  MI_M,MI_M,MI_M,MI_M,MI_M,MI_M,MI_M,SOL_M,DO_M,RE_M,MI_M,
+  FA_M,FA_M,FA_M,FA_M,FA_M,MI_M,MI_M,MI_M,MI_M,SOL_M,SOL_M,FA_M,RE_M,DO_M
+};
 
 // note durations: 4 = quarter note, 8 = eighth note, etc.:
 //整型数组保存歌曲we wish you a merry christmas所有音符的节奏
@@ -47,6 +53,14 @@ int noteDurations[] = {
   2, 2, 2, 2, 1, 2, 2, 2, 2, 2,
   2, 2, 2, 2, 2, 2, 4, 4, 2, 2, 2, 1
 };
+int noteDurations1[] = {
+  4,4,2,4,4,2,4,4,4,4,1,
+  4,4,4,8,4,4,4,8,8,4,4,4,4,2,2,
+  4,4,2,4,4,2,4,4,4,4,1,
+  4,4,4,8,4,4,4,8,8,4,4,4,4,2
+  };
+
+  
 
 void setup() {
 
@@ -55,6 +69,7 @@ void setup() {
 void loop() {
   // 通过for循环安顺序和节奏播放we wish you a merry christmas， 每个循环播放一个音符
   // thisNote的数值表示当前循环次数，从零开始
+  
   for (int thisNote = 0; thisNote < 52; thisNote++) {
 
     //根据节奏数值来计算音符的持续时间, 1000除以节奏代码，比如4分音符就是1000/4=250毫秒
@@ -68,6 +83,22 @@ void loop() {
   }
 
   //两次播放之间间隔10秒钟（10000毫秒）
-  delay(10000);
+  delay(3000);
+  
+  
+  for (int thisNote = 0; thisNote < 51; thisNote++) {
+
+    //根据节奏数值来计算音符的持续时间, 1000除以节奏代码，比如4分音符就是1000/4=250毫秒
+    int noteDuration = 1000 / noteDurations1[thisNote]; 
+
+    //在9通道输出音符melody[thisNote]，并持续noteDuration毫秒
+    tone(9, melody1[thisNote], noteDuration);
+    
+    //每个音符和下一个音符之间间隔30毫秒，可能是为了等蜂鸣器的震动完全停止，这样不会干扰下一个音符的播放
+    delay(noteDuration + 30);
+  }
+
+  //两次播放之间间隔10秒钟（10000毫秒）
+  delay(3000);
   
 }
