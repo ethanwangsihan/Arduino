ba77d636fa5edbeeacdac609e12153cdc5fece6d
ethanwangsihan <ethan.wang.sihan@hotmail.com>
2020-12-09 22:16:52 +0800

    calculate array length

--- Christmas_machine/music_light_show/music_light_show.ino
+++ Christmas_machine/music_light_show/music_light_show.ino
@@ -1,3 +1,6 @@
+//定义宏用来求出数组长度
+#define GET_ARRAY_LEN(array,len){len = (sizeof(array) / sizeof(array[0]));}
+
 //预定义音符常量到频率数值
 #define DO_L  262
 #define RE_L  294
@@ -53,6 +56,7 @@ int noteDurations[] = {
   2, 2, 2, 2, 1, 2, 2, 2, 2, 2,
   2, 2, 2, 2, 2, 2, 4, 4, 2, 2, 2, 1
 };
+
 int noteDurations1[] = {
   4,4,2,4,4,2,4,4,4,4,1,
   4,4,4,8,4,4,4,8,8,4,4,4,4,2,2,
@@ -60,14 +64,16 @@ int noteDurations1[] = {
   4,4,4,8,4,4,4,8,8,4,4,4,4,2
   };
 
-  
+int lightPin[] = {
+  2,3,12
+};
+
 
 void setup() {
   pinMode(9,OUTPUT);
   pinMode(2,OUTPUT);
   pinMode(3,OUTPUT);
   pinMode(12,OUTPUT);
-
 
 }
 
@@ -156,24 +162,34 @@ void loop() {
   //音乐部分
   // 通过for循环安顺序和节奏播放we wish you a merry christmas， 每个循环播放一个音符
   // thisNote的数值表示当前循环次数，从零开始
+
+  int lightIdx=0;
+
+  //计算melody数组长度，并保存到整型变量melodyCount中
+  int melodyCount;
+  GET_ARRAY_LEN(melody,melodyCount);
   
-  for (int thisNote = 0; thisNote < 52; thisNote++) {
+  for (int thisNote = 0; thisNote < melodyCount; thisNote++) {
 
     //根据节奏数值来计算音符的持续时间, 1000除以节奏代码，比如4分音符就是1000/4=250毫秒
     int noteDuration = 1000 / noteDurations[thisNote]; 
 
+    
     //在9通道输出音符melody[thisNote]，并持续noteDuration毫秒
     tone(9, melody[thisNote], noteDuration);
+
     
     //每个音符和下一个音符之间间隔30毫秒，可能是为了等蜂鸣器的震动完全停止，这样不会干扰下一个音符的播放
     delay(noteDuration + 30);
+    
   }
 
   //两次播放之间间隔10秒钟（10000毫秒）
   delay(3000);
-  
-  
-  for (int thisNote = 0; thisNote < 51; thisNote++) {
+
+  //计算melody1数组长度，并保存到整型变量melodyCount中
+  GET_ARRAY_LEN(melody1,melodyCount);
+  for (int thisNote = 0; thisNote < melodyCount; thisNote++) {
 
     //根据节奏数值来计算音符的持续时间, 1000除以节奏代码，比如4分音符就是1000/4=250毫秒
     int noteDuration = 1000 / noteDurations1[thisNote]; 
