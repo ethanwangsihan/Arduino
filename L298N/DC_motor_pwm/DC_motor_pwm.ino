44bd91f6f5b5e091dd023ffbede0692e34a8cc77
ethanwangsihan <ocpwj@hotmail.com>
2021-01-03 13:13:05 +0800

    pwm motor

---
+++ L298N/DC_motor_pwm/DC_motor_pwm.ino
@@ -0,0 +1,100 @@
+
+#define IN3  2
+#define IN4  4
+#define ENB  6
+#define MINPWM 25
+#define MAXPWM 45
+
+void setup() {
+  //设置串口波特率
+  Serial.begin(9600);
+  
+  //设置引脚模式
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+  pinMode(ENB, OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  //////////////////////////////
+  //顺时针加速到最快MAXPWM
+  //////////////////////////////
+  int n = MINPWM;
+  digitalWrite(IN3, LOW); //IN1和IN2只要输出高电平信号或低电平信号就可以
+  digitalWrite(IN4, HIGH);
+
+  while (n < MAXPWM)
+  {
+    analogWrite(ENB, n); //ENA要输出PWM信号（0-255）
+    Serial.print("CW:");
+    Serial.println(n);
+    delay(500);
+    n++;
+  }
+
+  delay(500);
+  
+  //////////////////////////////
+  //顺时针到减速到MINPWM
+  //////////////////////////////
+  while (n > MINPWM)
+  {
+    analogWrite(ENB, n); //ENA要输出PWM信号（0-255）
+    Serial.print("CW:");
+    Serial.println(n);
+    delay(500);
+    n--;
+  }
+
+  //////////////////////////////
+  //完全停止
+  //////////////////////////////
+  digitalWrite(IN3, LOW); //IN1和IN2只要输出高电平信号或低电平信号就可以
+  digitalWrite(IN4, LOW);
+  Serial.println("Stop");
+  delay(500);
+
+  //////////////////////////////
+  //逆时针加速到最快MAXPWM
+  //////////////////////////////
+  digitalWrite(IN3, HIGH); //IN1和IN2只要输出高电平信号或低电平信号就可以
+  digitalWrite(IN4, LOW);
+
+  while (n < MAXPWM)
+  {
+    analogWrite(ENB, n); //ENA要输出PWM信号(0-255)
+    Serial.print("CCW:");
+    Serial.println(n);
+    delay(500);
+    n++;
+  }
+
+  delay(500);
+  
+  //////////////////////////////
+  //逆时针到减速到MINPWM
+  //////////////////////////////
+  while (n > MINPWM)
+  {
+    analogWrite(ENB, n); //ENA要输出PWM信号（0-255）
+    Serial.print("CCW:");
+    Serial.println(n);
+    delay(500);
+    n--;
+  }
+
+  delay(500);
+  
+  //////////////////////////////
+  //完全停止
+  //////////////////////////////
+  digitalWrite(IN3, LOW); //IN1和IN2只要输出高电平信号或低电平信号就可以
+  digitalWrite(IN4, LOW);
+  Serial.println("Stop");
+  delay(500);
+
+
+}
