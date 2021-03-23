4dc8c078ba1c7511e330378fe74977cc224b6048
ethanwangsihan <ocpwj@yahoo.com>
2021-03-23 14:01:53 +0800

    irremote

---
+++ IRremote/IRremote/IRremote.ino
@@ -0,0 +1,21 @@
+#include <IRremote.h>
+//has to user IRremote 2.0.1
+int RECV_PIN = A1;
+
+IRrecv irrecv(RECV_PIN);
+
+decode_results results;
+
+void setup()
+{
+  Serial.begin(9600);
+  irrecv.enableIRIn(); // Start the receiver
+}
+
+void loop() {
+  if (irrecv.decode(&results)) {
+    Serial.println(results.value, HEX);
+    irrecv.resume(); // Receive the next value
+  }
+  delay(100);
+}
