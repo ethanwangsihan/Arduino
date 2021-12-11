b4d6707986e32fb97fc7ffb6b1a564d8abf2ea2e
ethanwangsihan <64050740+ethanwangsihan@users.noreply.github.com>
2021-12-11 23:09:14 +0800

    Update ps2stick.ino
    test

--- ps2_stick/ps2stick/ps2stick.ino
+++ ps2_stick/ps2stick/ps2stick.ino
@@ -1,8 +1,8 @@
 #define VRX A1
 #define VRY A2
 #define SW 6
-int resetx,resety;
-int jox, joy, jos;
+int startx,starty;
+int jox, joy;
 int outx, outy;
 int loopdelay=10;
 void setup() {
@@ -12,25 +12,40 @@ void setup() {
   Serial.begin(9600);
 
   //得到摇杆在中位是的数值
-  resetx=analogRead(VRX); 
-  resety=analogRead(VRY);
-  
+  startx=analogRead(VRX); 
+  starty=analogRead(VRY);
+
+
 }
 void loop() {
-  
+
   
   jox = analogRead(VRX);//读取x轴的数值，范围为0~1023
-  outx=map(jox-resetx, 0,1023,0,255);
-  
   joy = analogRead(VRY);//读取y轴的数值，范围为0~1023
-  outy=map(joy-resety, 0,1023,0,255);
-  jos = digitalRead(SW); //读取按键的数值，为0和1
-  
-  Serial.print("X=");
+
+  //x
+  if (jox>startx)
+  {
+    outx=map(jox,startx,1023,0,15);
+  }
+  else
+  {
+    outx=map(jox,0,startx,-15,0);
+  }
+  //y
+  if (joy>starty)
+  {
+    outy=map(joy,starty,1024,0,-15);
+  }
+  else
+  {
+    outy=map(joy,0,starty,15,0);
+  }
+
+  Serial.print("outX:");
   Serial.print(outx);
-  Serial.print(" |Y=");
-  Serial.print(outy);
-  Serial.print(" |Z=");
-  Serial.println(jos);
+  Serial.print(",outY:");
+  Serial.println(outy);
+  
   delay(loopdelay);
 }
